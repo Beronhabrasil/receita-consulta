74a7051e98c5c0d20b203242129613278ff209bb
Calheiro <33173643+CesarCalheiro@users.noreply.github.com>
2019-10-06 18:58:14 +0200

    Update Receita.php

--- app/Http/Controllers/Receita.php
+++ app/Http/Controllers/Receita.php
@@ -16,7 +16,7 @@ namespace App\Http\Controllers;
 class Receita {
  
   private  function api(){
-   $cnpj = 27865757000102;    
+   $cnpj = 18387246000123 ;    
    $client = new \GuzzleHttp\Client();
    $response = $client->request('GET', 'https://www.receitaws.com.br/v1/cnpj/'.$cnpj);
    return $response;
@@ -24,7 +24,21 @@ class Receita {
    public function getResult(){
    $sortout = $this->api();
    $getitfuck =  json_decode($sortout->getBody(), true);
-   dd($getitfuck);
-   }
+        foreach ($getitfuck['atividade_principal'] as $key => $value) {
+         echo  $key[0] ."  atividade ===>".$value['text'];
+         echo '<br>';
+         echo  $key[0] ."  codigo ===>".$value['code']."<br>";
+        }
+        echo "situacao &nbsp;&nbsp;". $getitfuck['situacao']."<br>";
+        echo "bairro &nbsp;&nbsp;". $getitfuck['bairro']."<br>";
+        echo "logradouro&nbsp;&nbsp;". $getitfuck['logradouro']."<br>";
+        echo "numero &nbsp;&nbsp;".$getitfuck['numero']."<br>";
+        echo "cep &nbsp;&nbsp;".$getitfuck['cep']."<br>";
+        echo "natureza_juridica  &nbsp;&nbsp;".$getitfuck['natureza_juridica']."<br>";
+        echo "fantasia &nbsp;&nbsp;".$getitfuck['fantasia']."<br>";
+        echo "cnpj &nbsp;&nbsp;".$getitfuck['cnpj']."<br>";
+        echo "ultima_atualizacao &nbsp;&nbsp;".$getitfuck['ultima_atualizacao']."<br>";
+        echo "tipo &nbsp;&nbsp;".$getitfuck['tipo']."<br>";
+    }
  
 }
